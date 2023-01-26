d19176cb10084e1684f1559eaea3dfccadedc4a1
Ivan Muktić <ivan@propeller.hr>
2023-01-26 08:38:33 +0100

    Setting up project, Getting gitignore, swiftformat.

---
+++ Sources/ContentView.swift
@@ -0,0 +1,17 @@
+import SwiftUI
+
+struct ContentView: View {
+    @State
+    private var query = ""
+    var body: some View {
+        NavigationView {
+            VStack {}
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Sources/GitHubRepoApp.swift
@@ -0,0 +1,10 @@
+import SwiftUI
+
+@main
+struct GitHubRepoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
