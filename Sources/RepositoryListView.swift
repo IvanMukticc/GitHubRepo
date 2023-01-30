ad9146e6c446f439b01c11558eabf251c37becb8
Ivan Muktić <ivan@propeller.hr>
2023-01-30 15:06:10 +0100

    Adding view model for RepositoryListView.

--- Sources/RepositoryListView.swift
+++ Sources/RepositoryListView.swift
@@ -1,54 +1,39 @@
 import SwiftUI
 
 struct RepositoryListView: View {
-    let apiManager = ApiManager()
-    @State
-    var repositories = [Repository]()
-    @State
-    private var searchTerm: String = ""
-    @State
-    private var errorAlertPresented = false
-    @State
-    private var errorMessage = ""
-    @State
-    private var timeRemaining = 1
-    let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
-    @State
-    var shouldTriggerRequest = false
-    
+    @StateObject
+    var viewModel: ViewModel
+
     var body: some View {
         NavigationView {
             List {
-                ForEach(repositories) { repository in
+                ForEach(viewModel.repositories) { repository in
                     Text(repository.name)
                 }
             }
-            .searchable(text: $searchTerm, prompt: "Please enter search term")
-            .onReceive(timer) { _ in
-                if timeRemaining > 0 {
-                    timeRemaining -= 1
+            .searchable(
+                text: $viewModel.searchTerm,
+                prompt: "Please enter search term"
+            )
+            .onReceive(viewModel.timer) { _ in
+                if viewModel.timeRemaining > 0 {
+                    viewModel.timeRemaining -= 1
                 } else {
-                    if shouldTriggerRequest {
-                        shouldTriggerRequest = false
-                        Task {
-                            do {
-                                repositories = try await apiManager
-                                    .getRepositories(for: searchTerm)
-                            } catch {
-                                errorAlertPresented = true
-                                errorMessage =
-                                "Could not find any data on \(searchTerm). Please try something else."
-                            }
-                        }
+                    if viewModel.shouldTriggerRequest {
+                        viewModel.shouldTriggerRequest = false
+                        viewModel.fetchData()
                     }
                 }
             }
-            .onChange(of: searchTerm, perform: { _ in
-                shouldTriggerRequest = true
-                timeRemaining = 1
+            .onChange(of: viewModel.searchTerm, perform: { _ in
+                viewModel.shouldTriggerRequest = true
+                viewModel.timeRemaining = 1
             })
-            .alert(isPresented: $errorAlertPresented) {
-                Alert(title: Text("Error"), message: Text(errorMessage))
+            .alert(isPresented: $viewModel.errorAlertPresented) {
+                Alert(
+                    title: Text("Error"),
+                    message: Text(viewModel.errorMessage)
+                )
             }
         }
     }
@@ -56,6 +41,6 @@ struct RepositoryListView: View {
 
 struct RepositoryListView_Previews: PreviewProvider {
     static var previews: some View {
-        RepositoryListView()
+        RepositoryListView(viewModel: RepositoryListView.ViewModel())
     }
 }
---
+++ Sources/RepositoryListViewModel.swift
@@ -0,0 +1,36 @@
+import Foundation
+import SwiftUI
+
+extension RepositoryListView {
+    @MainActor
+    class ViewModel: ObservableObject {
+        let apiManager = ApiManager()
+        @Published
+        var repositories = [Repository]()
+        @Published
+        var searchTerm: String = ""
+        @Published
+        var errorAlertPresented = false
+        @Published
+        var errorMessage = ""
+        @Published
+        var timeRemaining = 1
+        let timer = Timer.publish(every: 1, on: .main, in: .common)
+            .autoconnect()
+        @Published
+        var shouldTriggerRequest = false
+
+        func fetchData() {
+            Task {
+                do {
+                    repositories = try await apiManager
+                        .getRepositories(for: searchTerm)
+                } catch {
+                    errorAlertPresented = true
+                    errorMessage =
+                        "Could not find any data on \(searchTerm). Please try something else."
+                }
+            }
+        }
+    }
+}
